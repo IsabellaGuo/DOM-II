c17c86dd6ca567a13719a68515be818ef10dce0c
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-03 13:25:10 -0800

    mouseover effect

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// mouseover
+const logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("mouseover", () => {
+   logoHeading.classList.toggle("change-color");
+});
+
+logoHeading.addEventListener("mouseout", () => {
+   logoHeading.classList.toggle("change-color2");
+});
