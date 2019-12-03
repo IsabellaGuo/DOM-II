8e7a4b87fcf93658789572881e4be738d0da948f
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-03 15:02:21 -0800

    click done

--- js/index.js
+++ js/index.js
@@ -4,7 +4,15 @@ const logoHeading = document.querySelector(".logo-heading");
 logoHeading.addEventListener("mouseover", () => {
    logoHeading.classList.toggle("change-color");
 });
-
+// mouseout
 logoHeading.addEventListener("mouseout", () => {
    logoHeading.classList.toggle("change-color2");
 });
+
+// keydown
+// click
+document.querySelector(".text-content h2").addEventListener("click", myClick)
+
+function myClick(){
+   document.querySelector(".text-content h2").textContent = "Where To?"
+}
