1f9c725e7e18208034e9b192a810335e8525b690
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-03 16:45:37 -0800

    still have two more to go

--- js/index.js
+++ js/index.js
@@ -10,9 +10,41 @@ logoHeading.addEventListener("mouseout", () => {
 });
 
 // keydown
+document.querySelector("body").addEventListener("keydown", myKeydownFunc);
+
+function myKeydownFunc(){
+   alert(`You pressed a key!`);
+}
+
+
 // click
 document.querySelector(".text-content h2").addEventListener("click", myClick)
 
 function myClick(){
    document.querySelector(".text-content h2").textContent = "Where To?"
 }
+
+//load
+window.addEventListener("load", function(event){
+   alert (`Ready? Go!`);
+})
+
+// mousemove
+const destination = document.querySelector(".content-destination");
+destination.addEventListener("mousemove", () => {
+   destination.classList.toggle("change-color");
+})
+
+// resize
+window.addEventListener("resize", myResizeFunc)
+function myResizeFunc () {
+   alert(`Don't resize me`)
+}
+
+//dblclick
+const funintheSun = document.querySelector(".destination h4");
+
+funintheSun.addEventListener("dblclick", (event) =>{
+   funintheSun.classList.toggle("change-color3");
+});
+
